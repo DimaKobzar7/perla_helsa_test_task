d86cd4b569c98ec94f811b8be0cebb9eec13be1d
Dima Kobzar <kobzar7dima@gmail.com>
2022-12-28 21:00:05 +0200

    added counter and btn

---
+++ components/TheButton.vue
@@ -0,0 +1,13 @@
+<!-- надо менять название так как компонент не уникален -->
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="card">
+        <h1>Омега-3 <span>Тріска</span></h1>
+        <span>з вітамінами А і D3</span>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style></style>
---
+++ components/TheCounter.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="counter">
+    <button class="counter__btn">-</button>
+    <div class="counter__number">1</div>
+    <button class="counter__btn">+</button>
+  </div>
+</template>
+
+<style>
+.counter {
+  display: flex;
+}
+
+.counter__btn {
+  padding: 17px;
+}
+.counter__number {
+  font-size: 28px;
+  font-weight: 400;
+  line-height: 1.25;
+
+  /* text-align: center; */
+
+  color: #00284f;
+}
+</style>
