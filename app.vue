c97c310d692b83a65ca134b72671ddbaf3b69ed6
Dima Kobzar <dimakobzar@Dimas-MacBook-Pro.local>
2022-12-28 18:10:03 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,65 @@
+<template>
+  <main>
+    <section>
+      <div class="label">
+        <div class="label__text">
+          <img src="./assets/svg/drugs.svg" />
+          <p class="header__text">Безкоштовна досавка від 1800 грн</p>
+          <img src="./assets/svg/drugs.svg" />
+        </div>
+      </div>
+      <div class="container">
+        <!-- <NuxtWelcome /> -->
+        <TheHeader />
+      </div>
+    </section>
+  </main>
+</template>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+/* оно должно само подтягиватся есть спец папка для этого */
+/* есть и 2й шрифт Piazzolla */
+@font-face {
+  font-family: "Onest", sans-serif;
+  src: url("./assets/fonts/onest/TTF/OnestRegular1602-hint.ttf");
+  font-weight: 400;
+}
+
+@font-face {
+  font-family: "Onest", sans-serif;
+  src: url("./assets/fonts/onest/TTF/OnestMedium1602-hint.ttf");
+  font-weight: 500;
+}
+
+@font-face {
+  font-family: "Onest", sans-serif;
+  src: url("./assets/fonts/onest/TTF/OnestBold1602-hint.ttf");
+  font-weight: 700;
+}
+
+@font-face {
+  font-family: "Onest", sans-serif;
+  src: url("./assets/fonts/onest/TTF/OnestExtraBold1602-hint.ttf");
+  font-weight: 900;
+}
+
+body {
+  margin: 0;
+  /* font-family: Grtsk Giga;
+  font-weight: 600; */
+  overflow: hidden;
+}
+
+/* поменяй название */
+.label {
+  background-color: #99afff;
+}
+
+.label__text {
+  font-size: 50px;
+}
+</style>
---
+++ components/TheHeader.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="header">
+    <!-- <div class="header__top">
+      <img src="../assets/svg/drugs.svg" />
+      <p class="header__text">Безкоштовна досавка від 1800 грн</p>
+      <img src="../assets/svg/drugs.svg" />
+    </div> -->
+    <div class="header__middle">
+      <img src="../assets/svg/burger-menu.svg" />
+      <img src="../assets/svg/search.svg" />
+      <img src="../assets/svg/logo.svg" />
+      <img src="../assets/svg/phone.svg" />
+      <img src="../assets/svg/cart.svg" />
+    </div>
+    <div class="header__bottom">
+      <nav>
+        <a href="#">Омега-3</a>
+        <a href="#">Вітаміни</a>
+        <a href="#">Добавки</a>
+        <a href="#">Доставка</a>
+        <a href="#">Про нас</a>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<!-- из другой папки пока стили не тянет -->
+<style>
+.header {
+  /* background-color: #99afff; */
+}
+
+.header__text {
+}
+</style>
