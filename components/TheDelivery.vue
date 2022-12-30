74bb0b5deb453a86e8da281659b2146f69e2fab4
Dima Kobzar <kobzar7dima@gmail.com>
2022-12-30 21:59:30 +0200

    delivery done

--- components/TheDelivery.vue
+++ components/TheDelivery.vue
@@ -3,15 +3,20 @@
     <div class="container">
       <div class="delivery">
         <div class="delivery__item">
-          <img class="delivery__icon" src="../assets/svg/car.svg" alt="" />
+          <div><img class="delivery__icon" src="../assets/svg/car.svg" alt="" /></div>
+          <!-- <img class="delivery__icon" src="../assets/svg/car.svg" alt="" /> -->
           <p class="delivery__text">Безкоштовна доставка від 1800 грн</p>
         </div>
         <div class="delivery__item">
-          <img class="delivery__icon" src="../assets/svg/box.svg" alt="" />
+          <div><img class="delivery__icon" src="../assets/svg/box.svg" alt="" /></div>
+          <!-- <img class="delivery__icon" src="../assets/svg/box.svg" alt="" /> -->
           <p class="delivery__text">Відправки в день оформлення замовлення до 16:00</p>
         </div>
         <div class="delivery__item">
-          <img class="delivery__icon" src="../assets/svg/guarantee.svg" alt="" />
+          <div>
+            <img class="delivery__icon" src="../assets/svg/guarantee.svg" alt="" />
+          </div>
+          <!-- <img class="delivery__icon" src="../assets/svg/guarantee.svg" alt="" /> -->
           <p class="delivery__text">Гарантія повернення грошових коштів</p>
         </div>
         <!-- пока оставлю 1й вариант так как тут надо менять бутстраповские брейкпоинты и если в карточке не придется их менять то оставлю 1й вариант -->
