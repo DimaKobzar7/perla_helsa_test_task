19f62e7ec9709df766bc669a2359df2d7bcf9e5c
Dima Kobzar <kobzar7dima@gmail.com>
2022-12-31 21:54:46 +0200

    tabs completed

--- components/ProductTabs.vue
+++ components/ProductTabs.vue
@@ -15,7 +15,7 @@
   </ul>
   <div class="tab-content" id="myTabContent">
     <div class="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
-      <p class="nav-tabs-text">Комплекс EPA + DHA кислот і натуральних вітамінів А & D3 для здоров'я твоєї шкіри, волосся, зору, сну, міцних кісток, суглобів та імунітету.</p>
+      <p ref="truncation" class="nav-tabs-text">Комплекс EPA + DHA кислот і натуральних вітамінів А & D3 для здоров'я твоєї шкіри, волосся, зору, сну, міцних кісток, суглобів та імунітету.</p>
     </div>
     <div class="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">
       <ul class="nav-tabs-list">
@@ -29,72 +29,7 @@
       </ul>
     </div>
     <div class="tab-pane fade" id="contact-tab-pane" role="tabpanel" aria-labelledby="contact-tab" tabindex="0">
-      <!-- <table class="flexbe-table" style="-webkit-font-smoothing: antialiased; border-collapse: collapse; border-spacing: 0px; height: 1px; min-width: 100%; outline: none; table-layout: fixed; text-align: center">
-                  <tbody>
-                    <tr>
-                      <td style="height: 58px; text-align: var(--elementTextAlign, inherit); width: var(--colWidth, auto)">
-                        <p style="text-align: left"><strong>Склад:</strong></p>
-                      </td>
-                      <td style="height: 58px; text-align: var(--elementTextAlign, inherit); width: var(--colWidth, auto)">
-                        <p style="text-align: left">100% риб'ячий жир з печінки тріски, желатин, гліцерин.</p>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td style="height: 27px; text-align: var(--elementTextAlign, inherit)">
-                        <p style="text-align: left">
-                          <strong><strong>EPA / DHA</strong></strong>
-                        </p>
-                      </td>
-                      <td style="height: 27px; text-align: var(--elementTextAlign, inherit)">
-                        <p style="text-align: left">170 мг / 205 мг</p>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td style="height: 27px; text-align: var(--elementTextAlign, inherit)">
-                        <p style="text-align: left"><strong>Вітамін-А</strong></p>
-                      </td>
-                      <td style="height: 27px; text-align: var(--elementTextAlign, inherit)">
-                        <p style="text-align: left">656 мкг 2187&nbsp;МО</p>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td style="height: 27px; text-align: var(--elementTextAlign, inherit)">
-                        <p style="text-align: left"><strong>Вітамін-D3</strong></p>
-                      </td>
-                      <td style="height: 27px; text-align: var(--elementTextAlign, inherit)">
-                        <p style="text-align: left">5.2 мкг 210&nbsp;МО</p>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table> -->
       <ProductTable />
-      <!-- <table class="table">
-        <tbody>
-          <tr>
-            <th scope="row">Склад:</th>
-            <td></td>
-            <td></td>
-            <td>100% риб'ячий жир з печінки тріски, желатин, гліцерин</td>
-          </tr>
-          <tr>
-            <th scope="row">EPA / DHA</th>
-            <td></td>
-            <td></td>
-            <td>170 мг / 205 мг</td>
-          </tr>
-          <tr>
-            <th scope="row">Вітамін-А</th>
-            <td></td>
-            <td></td>
-            <td>656 мкг 2187 МО</td>
-          </tr>
-          <tr>
-            <th scope="row">Вітамін-D3</th>
-            <td colspan="2">Larry the Bird</td>
-            <td>5.2 мкг 210 МО</td>
-          </tr>
-        </tbody>
-      </table> -->
     </div>
     <div class="tab-pane fade" id="disabled-tab-pane" role="tabpanel" aria-labelledby="disabled-tab" tabindex="0">
       <p class="nav-tabs-text mb-4">Для профілактики, дорослим і дітям від 11 років – <strong class="fst-italic">4 капсули на день</strong>, разом із їжею або одразу після. Бажано з іншими жирами для кращого засвоєння.</p>
@@ -102,3 +37,47 @@
     </div>
   </div>
 </template>
+
+<script setup>
+let truncation = ref(null);
+
+onMounted(() => {
+  let truncationText = truncation.value.textContent;
+  //   console.log(truncationText.length > 83);
+  //   почему то переменную оно не воспринимает
+  console.log(window.innerWidth);
+  //   вроде норм но надо перезагружать чтобы условие работало так как произошел рендер и window.innerWidth не меняется
+  // и оно может и не надо так как пользователь не должен менять разрешения своего экрана
+  if (window.innerWidth < 768) {
+    truncation.value.textContent = truncationText.length > 83 ? `${truncationText.slice(0, 81)}…` : truncationText;
+  }
+  //   truncation.value.textContent = truncationText.length > 83 ? `${truncationText.slice(0, 81)}…` : truncationText;
+  // элемент DOM будет определён в ref после первоначальной отрисовки
+  //   console.log(truncation.value.textContent);
+  //   var str = truncation.value.textContent.slice(0, 83); //например макс 100 символов
+  //   var a = str.split(' ');
+  //   console.log(a);
+  //   a.splice(a.length - 1, 1);
+  //   str = a.join(' ');
+  //   //   console.log(str);
+  //   truncation = truncation.value.textContent = str + '...';
+  //   return truncation.value.textContent.length > 83 ? `${truncation.value.textContent.slice(0, 83)}…` : truncation.value.textContent;
+  //   console.log(truncation.value.textContent.length > 83 ? `${truncation.value.textContent.slice(0, 81)}…` : truncation.value.textContent);
+  //   truncation.value.textContent = str + '...';
+  //   truncation.value.textContent = truncation.value.textContent.length > 83 ? `${truncation.value.textContent.slice(0, 81)}…` : truncation.value.textContent;
+  watch: {
+    window.innerWidth;
+  }
+});
+
+// var str = truncation.value.textContent.slice(0, 100); //например макс 100 символов
+// var a = str.split(' ');
+// a.splice(a.length - 1, 1);
+
+// console.log(truncation.value);
+// var text = 'много много текстов ...';
+// var str = text.slice(0, 100); //например макс 100 символов
+// var a = str.split(' ');
+// a.splice(a.length - 1, 1);
+// str = a.join(' ');
+</script>
